6f379f46c82893a343b6ef238a610484a3deb18a
Mehmet Nabi Duru <75908758+mehmet-nabi-duru@users.noreply.github.com>
2024-04-03 21:14:22 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,12 +1,40 @@
-const http = require('http');
+// create web server
+// create comments
+// create getcomments
+// create deletecomments
+// create updatecomments
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+const comments = [];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/comments', (req, res) => {
+    const comment = req.body;
+    comments.push(comment);
+    res.send(comment);
 });
 
-const port = 3000;
-server.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+app.get('/comments', (req, res) => {
+    res.send(comments);
+});
+
+app.delete('/comments/:id', (req, res) => {
+    const id = req.params.id;
+    comments.splice(id, 1);
+    res.send(comments);
+});
+
+app.put('/comments/:id', (req, res) => {
+    const id = req.params.id;
+    comments[id] = req.body;
+    res.send(comments[id]);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
